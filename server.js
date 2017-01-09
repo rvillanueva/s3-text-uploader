1e7cb8b8f1eb9accb2f79a2e471c7bc0011c3df3
Ryan Villanueva <ryanvill@gmail.com>
2017-01-09 01:10:02 -0500

    Updated server to connect with AWS.

--- server.js
+++ server.js
@@ -4,37 +4,42 @@ var AWS = require('aws-sdk');
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
+var path = require('path');
+
 var app = express();
 
 var surveyUrl = 'https://s3.amazonaws.com/wfp-bot/survey.yml';
-
-app.get('api/survey', (req, res) => {
-  request.get(surveyUrl).success(data =>{
-    return res.status(200).send(data)
-  }).error(err => {
-    console.log(err);
-    return res.status(err.statusCode || 500).send(err)
-  })
-})
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
-app.post('api/publish', (req, res) => {
+app.get('/api/survey', function(req, res) {
+  request.get(surveyUrl, function(err, response, body){
+    if(!err){
+      return res.status(200).send(body)
+    } else {
+      console.log(err);
+      return res.status(err.statusCode || 500).send(err)
+    }
+  })
+})
+
+app.post('/api/publish', function(req, res) {
   var creds = new AWS.Credentials({
     accessKeyId: req.body.key, secretAccessKey: req.body.secret
   })
-  var s3 = new AWS.s3({
+  var s3 = new AWS.S3({
     credentials: creds
   })
 
   s3.putObject({
     Bucket: req.body.bucket || 'wfp-bot',
     Key: req.body.filename || 'survey.yml',
-    Data: req.body.yaml || ''
+    Body: req.body.yaml || '',
+    ACL: 'public-read'
   }, function(err, data){
     if(!err){
       console.log('Successfully uploaded file.')
@@ -46,8 +51,10 @@ app.post('api/publish', (req, res) => {
   })
 })
 
-app.use(express.static(__dirname + '/'));
+app.use('/', express.static(path.join(__dirname, '/')));
 
-app.listen(3000, function(){
-  console.log('Server running, listening on 3000.')
-})
+// Listen for requests
+var server = app.listen(3000, function() {
+  var port = server.address().port;
+  console.log('Server running on port ' + port);
+});
